bf7844f79815d0af80536c8b5a5fa8cd23a717e5
Mabdille <mabdille97@gmail.com>
2021-06-10 13:28:50 +0100

    no message

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 //DOM ELEMENTS
 const time = document.getElementById('time'),
-greeting = document.getElementById('greeting'),
-name = document.getElementById('name'),
-focus = document.getElementById('focus);
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
     
 //show Time
 function showTime() {
-    let today = new Date(),
-        hour = today.getHours(),
-        min = today.getMinutes(),
-        sec = today.getSeconds();
-    }
-
+            let today = new Date(),
+                hour = today.getHours(),
+                min = today.getMinutes(),
+                sec = today.getSeconds();
     
+
+            // SET AM or PM 
+            const amPm = hour >= 12 ? 'PM' : 'AM'
+
+            // 12HR Format
+            hour = hour % 12 || 12
+    
+            // Output Time
+            time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`
+    
+            setTimeout(showTime, 1000)
+}
+        
+// Run
+showTime()
